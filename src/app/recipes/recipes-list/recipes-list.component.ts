bcd871c5c01c06ece4bd2578ba7793a0e9c965a8
Arafat Tai <tai.arafat.at@gmail.com>
2018-10-29 12:45:08 +0530

    completed basic UI set up

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -7,6 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipesListComponent implements OnInit {
 
+  recipes: Recipe[] = [
+    new Recipe('Biryani', 'The real biryani', `https://www.ndtv.com/cooks/images/dum.murg.ki.kacchi.biryani.jpg`),
+    new Recipe('Veg Biryani', 'Pulao kehte hai isse!!!', 'https://www.indianhealthyrecipes.com/wp-content/uploads/2014/09/veg-biryani-recipe.jpg')
+  ];
   constructor() { }
 
   ngOnInit() {
